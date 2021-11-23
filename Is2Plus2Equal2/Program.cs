98060744b1c488f8509e47ddd8d07a06f66b4b97
rothexD <56934789+rothexD@users.noreply.github.com>
2021-11-23 15:35:14 +0100

    project

---
+++ Is2Plus2Equal2/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Is2Plus2Equal4
+{
+    public static class Example
+    {
+        public static bool Is2Plus2Equal4Method()
+        {
+            if (2 + 2 == 4)
+            {
+                return true;
+            }
+            return false;
+        }
+    }
+    class Program
+    {
+        
+        static void Main(string[] args)
+        {
+            if (Example.Is2Plus2Equal4Method())
+            {
+                Console.WriteLine("Yes 2 plus 2 is 4");
+            }
+            else
+            {
+                Console.WriteLine("No 2 plus 2 is not 4");
+            }
+        }
+    }
+}
